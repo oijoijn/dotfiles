097827da83df1f01da00aa628e3a341db8de738e
soto <syuweb522@gmail.com>
2025-07-13 18:40:36 +0900

    telescope.luaの変更

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,20 +2,26 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+
+    -- Telescope が読み込まれた後に実行される
+    config = function(_, opts)
+        local telescope                  = require("telescope")
+        local actions                    = require("telescope.actions")
+
+        -- opts は下で定義したテーブルが渡ってくる
+        opts.defaults.mappings.i["<CR>"] = actions.select_tab
+        opts.defaults.mappings.n["<CR>"] = actions.select_tab
+
+        telescope.setup(opts)
+    end,
+
+    -- **純粋なデータ** だけにする。ここで `require` しない
     opts = {
         defaults = {
-            mappings = {
-                i = {
-                    -- InsertモードでEnterキーを押したときのアクションを変更
-                    ["<CR>"] = require("telescope.actions").select_tab,
-                },
-                n = {
-                    -- NormalモードでEnterキーを押したときのアクションを変更
-                    ["<CR>"] = require("telescope.actions").select_tab,
-                },
-            },
+            mappings = { i = {}, n = {} },
         },
     },
+
     keys = {
         { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Telescope: find files" },
         { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Telescope: live grep" },
